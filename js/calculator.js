bd439e12eded2165421a20b7a0c899a5508ff80c
LuzSallietti <luzsallietti@gmail.com>
2021-05-20 13:06:16 -0300

    functions js and css

--- js/calculator.js
+++ js/calculator.js
@@ -21,54 +21,96 @@ let bottlePrice = 250;
 const date = document.getElementById("date");
 const partyTheme = document.getElementById("partyTheme");
 const currency = document.getElementById("currency");
+let curr_symbol = document.getElementById("curr_symbol");
 let amount = document.getElementById("amount");
 let total = 0;
+let usd_total = 0;
+let euro_total = 0;
+let msg;
+let final_budget;
+let final_currency;
 const confirmButton = document.getElementsByTagName("button");
 const checkboxes = document. querySelectorAll('input[type="checkbox"]');
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', () => {
-        total = 0;        
+        msg = "Hola! Quiero confirmar mi presupuesto. Estos son los diseños que necesito: ";
+        total = 0;
+        currency.value="pesoArg";
+        curr_symbol.innerText = "$";                
         if (invitation.checked){
-            total += invPrice;                       
+            total += invPrice;
+            msg += "invitación/";                                            
         } 
         if (tag.checked) {
-            total += tagPrice;           
+            total += tagPrice;
+            msg += "tag cierrabolsita/";                        
         }
         if (card.checked){
             total += cardPrice;
+            msg +="tarjeta o estampita/";
+
         }
         if (pennants.checked){
             total += pennPrice;
+            msg +="banderines/";
         }
         if (banner.checked){
             total += bannPrice;
+            msg +="cartel de Bienvenida/";
         }
         if (popcornBox.checked){
             total += popPrice;
+            msg +="pochoclera/";
         }
         if (milkBox. checked){
             total += milkPrice;
+            msg +="milkbox/";
         }
         if (roundLabels.checked){
             total+= roundPrice;
+            msg +="círculos multiuso/";
         }
         if (candyLabels.checked){
             total += candyPrice;
+            msg +="envoltorios para candy bar/";
         }
         if (bottleLabels.checked){
             total += bottlePrice;
+            msg +="fajas para botellas/";
         }         
-         amount.innerHTML=total; 
+         amount.innerHTML=total;
+         console.log(msg); 
     })
 })
-
-/*invitation.addEventListener('change', () =>{
-    if (invitation.checked){
-        total = total+ 990;
-        console.log(total);
-    } else {
-        total = total-990;
-        console.log(total);
+currency.addEventListener('change', () => {
+    if(currency.value === "pesoArg"){
+        total = total;
+        amount.innerHTML = total;
+        final_budget = total;
+        final_currency = "pesos";
     }
-})*/
+    if(currency.value === "dolar"){
+        usd_total = Math.round(total/68);
+        amount.innerHTML = usd_total;
+        final_budget = usd_total;
+        final_currency = "dólares";
+        curr_symbol.innerText = "$";
+    }
+    if (currency.value === "euro"){
+        euro_total = Math.round(total/75);
+        amount.innerHTML = euro_total;
+        final_budget = euro_total;
+        final_currency = "euros"; 
+        curr_symbol.innerText = "€";       
+    }
+})
+
+confirmButton[0].addEventListener('click', (e) => {
+    e.preventDefault();
+    let delivery_date = date.value;
+    let theme = partyTheme.value;
+    console.log(msg);    
+    window.open(`https://wa.me/5493574437945?text=${msg}%20El%20precio%20es%20${final_budget}%20${final_currency}%20y%20la%20temática%20que%20elegí%20es%20${theme}.%20Necesito%20los%20diseños%20para%20esta%20fecha:%20${delivery_date}`, '_blank');
+});
+
